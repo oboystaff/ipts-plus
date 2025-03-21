bba7f96da67449fc2c8c1f0b81069bc5e2953219
mustapha <mustaph@noreply.com>
2025-03-21 09:27:45 +0000

    new updates

--- app/Http/Controllers/Building/BuildingController.php
+++ app/Http/Controllers/Building/BuildingController.php
@@ -32,6 +32,8 @@ class BuildingController extends Controller
 
             ini_set('upload_max_filesize', '100M');
             ini_set('post_max_size', '100M');
+            ini_set('max_execution_time', '300');
+            ini_set('memory_limit', '256M');
 
             $request->validate([
                 'file' => 'required|mimes:xlsx,csv',
